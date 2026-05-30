b08a7698fbe59e7ddfbb2bed4b9c5573df3b0376
Cheran <cheran.v@thewholewave.com>
2026-05-30 16:48:43 +0530

    Feat : Mobile Responsiveness

---
+++ src/assets/Icons/MenuIcon.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const MenuIcon = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="Frame" clip-path="url(#clip0_1_1111)">
+        <g id="Group">
+          <g id="Group_2">
+            <path
+              id="Vector"
+              d="M24 3.49597H0V6.92447H24V3.49597ZM24 10.287H0V13.7155H24V10.287ZM24 17.076H0V20.5045H24V17.076Z"
+              fill="white"
+            />
+          </g>
+        </g>
+      </g>
+      <defs>
+        <clipPath id="clip0_1_1111">
+          <rect width="24" height="24" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default MenuIcon;
